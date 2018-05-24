e94bbd5c2e12f3ac4dec23b877acec051442b01c
Zachary Passmore <fireampersand@live.com>
2018-05-24 01:36:26 -0600

    Added Comments to server.py

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import python.leds as led
 #Gloabl Variable for current Pattern
 CURRENT_PATTERN = "No Pattern Running"
 
+#Starting the flask app
 app = Flask(__name__)
 
 def loopPattern(type):
@@ -24,6 +25,7 @@ def loopPattern(type):
 
 @app.route("/")
 def index():
+    """The Defualt web page on the server"""
     templateData = {
         'title' : 'Led Pattern Status',
         'pattern' : CURRENT_PATTERN,
@@ -32,83 +34,115 @@ def index():
 
 @app.route("/<deviceName>/", methods=['GET', 'POST'])
 def action(deviceName):
+    """The Main route path for the server. Animations get selected here"""
     CURRENT_PATTERN = "Changing"
+    
+    #If The client is sending data back, run this if chunk. 
     if request.method == 'POST':
         #led.turnOff()
+        
+        #Getting the new brightness value from the Javascript, and printing the new value.
         newBright = int(request.form['bright'])
         print("Setting the new brightness to: " + str(newBright))
-        led.STRIP_BRIGHTNESS = newBright
+        
+        #Changing the gloabl brightness variable and telling the leds to update themselves.
+        led.STRIP_BRIGHTNESS = newBright 
         led.newBrightness()
          
+    #Checks to see what the user has requested to be run.
+    #Stops all patterns and turns the lights off
     if deviceName == 'stop':
         CURRENT_PATTERN = 'No Pattern Running'
         led.MASTER_LOOP = False
         led.turnOff()
 
+    #Runs random patterns with random colors (Name needs to change)    
     if deviceName == 'bootup':
+        
+        #If There is already a pattern running, turn the lights and Master loop off
         if led.MASTER_LOOP ==  True:
             led.MASTER_LOOP = False
-        led.turnOff()
+            led.turnOff()
         CURRENT_PATTERN = 'Color Cycle'
+        
+        #Restarting the Master Loop
         led.MASTER_LOOP = True
+        
+        #Threading to allow it to run in the background
         t = threading.Thread(target=loopPattern, args=("B"))
         t.daemon = True
         t.start()
 
+    #Runs the Theater Chase Animation    
     if deviceName == 'TheaterChase':
+        
+       #If There is already a pattern running, turn the lights and Master loop off
        if led.MASTER_LOOP == True:
            led.MASTER_LOOP = False
-       led.turnOff()
+           led.turnOff()
        CURRENT_PATTERN = "Theater Chase"
+    
+       #Restarting the Master Loop
        led.MASTER_LOOP = True
+    
+       #Threading to allow it to run in the background
        t = threading.Thread(target=loopPattern, args=("T"))
        t.daemon = True
        t.start()
-    
+   
+    #Sets all the leds to one static color
     if deviceName == 'StaticColor':
+        
+       #If There is already a pattern running, turn the lights and Master loop off 
        if led.MASTER_LOOP == True:
            led.MASTER_LOOP = False
-       led.turnOff()
+           led.turnOff()
        CURRENT_PATTERN = "Static Color"
+    
+       #Threading to allow it to run in the background
        t = threading.Thread(target=loopPattern, args=("S"))
        t.daemon = True
        t.start()
 
+    #Runs the Rave Animation, Flashing lights
     if deviceName == 'Rave':
+        
+        #If There is already a pattern running, turn the lights and Master loop off
         if led.MASTER_LOOP == True:
             led.MASTER_LOOP = False
-        led.turnOff()
+            led.turnOff()
         CURRENT_PATTERN = "Rave"
+        
+        #Restarting the Master Loop
         led.MASTER_LOOP = True
+        
+        #Threading to allow it to run in the background
         t = threading.Thread(target=loopPattern, args=("R"))
         t.daemon = True
         t.start()
 
-
+    #Runs the animation pong, ball bounces back and forth    
     if deviceName == 'Pong':
+        
+        #If There is already a pattern running, turn the lights and Master loop off 
         if led.MASTER_LOOP == True:
             led.MASTER_LOOP = False
-        led.turnOff()
+            led.turnOff()
         CURRENT_PATTERN = "Pong"
+        
+        #Restarting the Master Loop
         led.MASTER_LOOP = True
+        
+        #Threading to allow it to run in the background
         t = threading.Thread(target=loopPattern, args=("P"))
         t.daemon = True
         t.start()
 
+    #Creating a dictionary to send back to the webpage to use variables from this side.    
     templateData = {
         'title' : 'LED Pattern Status',
         'pattern' : CURRENT_PATTERN,
     }
-    return render_template('index.html', **templateData)
-
-@app.route('/brightnessUpdate', methods=['GET','POST'])
-def brightnessUpdate():
-    templateData = {
-        'tilte' : 'LED Pattern Status',
-        'pattern' : CURRENT_PATTERN,
-    }
-    print('In brightness')
-
     return render_template('index.html', **templateData)
 
 if __name__ == "__main__":

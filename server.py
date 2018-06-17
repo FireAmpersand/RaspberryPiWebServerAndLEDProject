94f98666a4af17fa12b98d4ed9f4bd0106e15412
Zachary Passmore <fireampersand@live.com>
2018-06-17 01:26:17 -0600

    Changed deviceName names to match index links

--- server.py
+++ server.py
@@ -5,25 +5,32 @@ import python.internalLEDs as iLED #Import for the inside leds
 import python.externalLEDs as eLED #Import for the outside leds
 
 #Gloabl Variable for current Pattern
-CURRENT_PATTERN = "No Pattern Running"
+CURRENT_INSIDE_PATTERN = "No Pattern Running"
+CURRENT_OUTSIDE_PATTERN = "No Pattern Running"
 
 #Starting the flask app
 app = Flask(__name__)
 
 def loopPattern(type):
     """Gets Called as a multiprocess and loops the code"""
-    if type == 'T':
-        iLED.runTheater()
-    elif type  == "B":
+    if type == 'A':
         iLED.runColorCycle()
-    elif type == "S":
-        iLED.staticColor(255,255,255)
-    elif type == "R":
-        iLED.runRave()
-    elif type == "P":
-        iLED.pong()
-    elif type == 't':
+    elif type == 'B':
+        eLED.runColorCycle()
+    elif type == 'C':
+        iLED.runTheater()
+    elif type == 'D':
         eLED.runTheater()
+    elif type == 'E':
+        iLED.staticColor(255,255,255)
+    elif type == 'F':
+        eLED.staticColor(255,255,255)
+    elif type == 'G':
+        iLED.runRave()
+    elif type == 'H':
+        eLED.runRave()
+    elif type == 'I':
+        iLED.pong()
 
 
 @app.route("/")
@@ -31,7 +38,8 @@ def index():
     """The Defualt web page on the server"""
     templateData = {
         'title' : 'Led Pattern Status',
-        'pattern' : CURRENT_PATTERN,
+        'patternInside' : CURRENT_INSIDE_PATTERN,
+        'patternOutside' : CURRENT_OUTSIDE_PATTERN,
         'lightToggle' : iLED.MOVIE_LIGHT,
     }
     return render_template('index.html',**templateData)
@@ -39,7 +47,6 @@ def index():
 @app.route("/<deviceName>/", methods=['GET', 'POST'])
 def action(deviceName):
     """The Main route path for the server. Animations get selected here"""
-    CURRENT_PATTERN = "Changing"
     
     #If The client is sending data back, run this if chunk. 
     if request.method == 'POST':
@@ -62,81 +69,150 @@ def action(deviceName):
        else:
           iLED.MOVIE_LIGHT = True
     
-    #Stops all patterns and turns the lights off
-    if deviceName == 'stop':
-        CURRENT_PATTERN = 'No Pattern Running'
+    #Stops all patterns and turns the lights off for the inside lights only
+    if deviceName == 'StopInside':
+        CURRENT_INSIDE_PATTERN = 'No Pattern Running'
         iLED.MASTER_LOOP = False
         iLED.turnOff()
+        
+    #Stops all patterns and turns the lights off for the outside lights only
+    if deviceName == 'StopOutside':
+        CURRENT_OUTSIDE_PATTERN = 'No Pattern Running'
         eLED.MASTER_LOOP = False
         eLED.turnOff()
+        
 
-    #Runs random patterns with random colors    
-    if deviceName == 'ColorCycle':
+    #Runs random patterns with random colors   
+    #Inside Command
+    if deviceName == 'ColorCycleInside':
         
         #If There is already a pattern running, turn the lights and Master loop off
         if iLED.MASTER_LOOP ==  True:
             iLED.MASTER_LOOP = False
             iLED.turnOff()
-        CURRENT_PATTERN = 'Color Cycle'
+        CURRENT_INSIDE_PATTERN = 'Color Cycle'
         
         #Restarting the Master Loop
         iLED.MASTER_LOOP = True
+        
+        #Threading to allow it to run in the background
+        t = threading.Thread(target=loopPattern, args=("A"))
+        t.daemon = True
+        t.start()
+     
+    #Outside Command
+    if deviceName == 'ColorCycleOutside':
+        
+        #If There is already a pattern running, turn the lights and Master loop off
+        if eLED.MASTER_LOOP ==  True:
+            eLED.MASTER_LOOP = False
+            eLED.turnOff()
+        CURRENT_OUTSIDE_PATTERN = 'Color Cycle'
+        
+        #Restarting the Master Loop
+        eLED.MASTER_LOOP = True
         
         #Threading to allow it to run in the background
         t = threading.Thread(target=loopPattern, args=("B"))
         t.daemon = True
         t.start()
 
-    #Runs the Theater Chase Animation    
-    if deviceName == 'TheaterChase':
+    #Runs the Theater Chase Animation
+    #Inside Command
+    if deviceName == 'TheaterChaseInside':
         
        #If There is already a pattern running, turn the lights and Master loop off
        if iLED.MASTER_LOOP == True:
            iLED.MASTER_LOOP = False
            iLED.turnOff()
-       CURRENT_PATTERN = "Theater Chase"
+       CURRENT_INSIDE_PATTERN = "Theater Chase"
     
        #Restarting the Master Loop
        iLED.MASTER_LOOP = True
+    
+       #Threading to allow it to run in the background
+       t = threading.Thread(target=loopPattern, args=("C"))
+       t.daemon = True
+       t.start()
+    
+    #Outside Command
+    if deviceName == 'TheaterChaseOutside':
+        
+       #If There is already a pattern running, turn the lights and Master loop off
+       if eLED.MASTER_LOOP == True:
+           eLED.MASTER_LOOP = False
+           eLED.turnOff()
+       CURRENT_OUTSIDE_PATTERN = "Theater Chase"
+    
+       #Restarting the Master Loop
        eLED.MASTER_LOOP = True
     
        #Threading to allow it to run in the background
-       t = threading.Thread(target=loopPattern, args=("T"))
+       t = threading.Thread(target=loopPattern, args=("D"))
        t.daemon = True
        t.start()
-
-       tt = threading.Thread(target=loopPattern, args=("t"))
-       tt.daemon = True
-       tt.start()
    
     #Sets all the leds to one static color
-    if deviceName == 'StaticColor':
+    #Inside Command
+    if deviceName == 'StaticColorInside':
         
        #If There is already a pattern running, turn the lights and Master loop off 
        if iLED.MASTER_LOOP == True:
            iLED.MASTER_LOOP = False
            iLED.turnOff()
-       CURRENT_PATTERN = "Static Color"
+       CURRENT_INSIDE_PATTERN = "Static Color"
     
        #Threading to allow it to run in the background
-       t = threading.Thread(target=loopPattern, args=("S"))
+       t = threading.Thread(target=loopPattern, args=("E"))
+       t.daemon = True
+       t.start()
+    
+    #Outside Command
+    if deviceName == 'StaticColorOutside':
+        
+       #If There is already a pattern running, turn the lights and Master loop off 
+       if eLED.MASTER_LOOP == True:
+           eLED.MASTER_LOOP = False
+           eLED.turnOff()
+       CURRENT_OUTSIDE_PATTERN = "Static Color"
+    
+       #Threading to allow it to run in the background
+       t = threading.Thread(target=loopPattern, args=("F"))
        t.daemon = True
        t.start()
 
     #Runs the Rave Animation, Flashing lights
-    if deviceName == 'Rave':
+    #Inside Command
+    if deviceName == 'RaveInside':
         
         #If There is already a pattern running, turn the lights and Master loop off
         if iLED.MASTER_LOOP == True:
             iLED.MASTER_LOOP = False
             iLED.turnOff()
-        CURRENT_PATTERN = "Rave"
+        CURRENT_INSIDE_PATTERN = "Rave"
         
         #Restarting the Master Loop
         iLED.MASTER_LOOP = True
         
         #Threading to allow it to run in the background
-        t = threading.Thread(target=loopPattern, args=("R"))
+        t = threading.Thread(target=loopPattern, args=("G"))
+        t.daemon = True
+        t.start()
+       
+    #Outside Command
+    if deviceName == 'RaveOutside':
+        
+        #If There is already a pattern running, turn the lights and Master loop off
+        if eLED.MASTER_LOOP == True:
+            eLED.MASTER_LOOP = False
+            eLED.turnOff()
+        CURRENT_OUTSIDE_PATTERN = "Rave"
+        
+        #Restarting the Master Loop
+        eLED.MASTER_LOOP = True
+        
+        #Threading to allow it to run in the background
+        t = threading.Thread(target=loopPattern, args=("H"))
         t.daemon = True
         t.start()
 
@@ -153,14 +229,15 @@ def action(deviceName):
         iLED.MASTER_LOOP = True
         
         #Threading to allow it to run in the background
-        t = threading.Thread(target=loopPattern, args=("P"))
+        t = threading.Thread(target=loopPattern, args=("I"))
         t.daemon = True
         t.start()
 
     #Creating a dictionary to send back to the webpage to use variables from this side.    
     templateData = {
         'title' : 'LED Pattern Status',
-        'pattern' : CURRENT_PATTERN,
+        'patternInside' : CURRENT_INSIDE_PATTERN,
+        'patternOutside' : CURRENT_OUTSIDE_PATTERN,
         'lightToggle' :iLED.MOVIE_LIGHT,
     }
     return render_template('index.html', **templateData)
